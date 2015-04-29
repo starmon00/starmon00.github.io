e6e2d50a65fd43619b142bdbcfab72259c9ef5bd
starmon00 <vcphng@hotmail.com>
2015-04-28 19:14:42 -0700

    updated w/ js
    js 1.0

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    document.getElementById("frm1").submit();
+    window.alert("BUY OUR GAMES");
+    window.alert("DO IT");
+    window.alert("DO IT NOW");
+    window.alert("RIGHT NOW");
+    window.alert("GIVE US ALL YOUR MONEY");
+    window.alert("DOOOO ITTTTT");
+    window.alert("SORRY");
+    window.alert("I didn't want to do this");
+    window.alert("I wanted to be");
+    window.alert("A LUMBERJACK");
+    window.alert("LEAPING FROM TREE TO TREE");
+    window.alert("OOOOOH I'M A LUMBERJACK AND I'M OKAY");
+    window.alert("I SLEEP ALL NIGHT AND I WORK ALL DAY");
+}
