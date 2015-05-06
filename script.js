3b1100d6a848be09db00090698fd4bd551993ad0
imcnicol <Ian@Ians-MacBook-Pro.local>
2015-05-06 07:42:06 -0700

    validate form

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function SubMit() {
     window.alert("Congradulations, you are now subscribed to our mailing list.");
 }
 
-
+function validateForm() {
+    var x = document.forms["myForm"]["frm1"].value;
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
