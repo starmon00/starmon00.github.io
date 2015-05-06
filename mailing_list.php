5210ce1aa55f689aa1b33116d6f0fd302450ec82
starmon00 <vcphng@hotmail.com>
2015-05-06 06:44:45 -0700

    more features
    looks more legit

---
+++ mailing_list.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+if(empty($name)||empty($email))
+{
+	echo "Name and email are mandatory!";
+	exit;
+}
+
+$email_from = 'vcphung@uci.edu';
+$email_subject = "Subscribed to Mailing List";
+$email_body = "Congradulations $name, you are not subscribed to Not Black Market Gaming mailing list! \n \n Thank You!".
+
+$to = "$email";
+$headers = "From: $email_from \r \n";
+$headers .= "Reply-To: $email \r \n";
+mail($to,$email_subject,$email_body,$headers)
