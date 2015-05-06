5bab655a2e409c5241356c613d788bdbfb4f5bf0
starmon00 <vcphng@hotmail.com>
2015-05-06 07:52:45 -0700

    fdsa
    fdsa

--- script.js
+++ script.js
@@ -3,4 +3,24 @@ function SubMit() {
     window.alert("Congradulations, you are now subscribed to our mailing list.");
 }
 
+function validateMailList(){
+	var formObj = document.getElementById("frm1");
+	var email = formObj.email.value;
 
+	if (!validateEmail(email))
+	{
+		alert("Error: Invaid E-mail, must have '@'.");
+		return false;
+	}
+
+
+}
+
+function validateEmail(email)
+{
+	var p == email.search([@]);
+	if (p == 0)
+		return true;
+	else
+		return false;
+}
